d96ea8b402f16493505f0e28a8821936721785a1
ErickDArx <alexalonso2706@gmail.com>
2021-05-09 20:25:36 -0600

    Avances

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,36 +16,61 @@
 @endsection
 
 @section('Portada')
+
 <div class="container mt-2">
     <h2 style="font-size: 1.1rem">Productos disponibles</h2>
 
-    <div class="form-group">
-        <form action="{{route('AgregarMascarilla')}}" method="POST" enctype="multipart/form-data">
-            @csrf
-        <label for="">Nombre de la mascarilla</label>
-        <input type="text">
-        <label for="">Categoria</label>
-        <input type="text">
-        <label for="">Precio</label>
-        <input type="text">
-        <label for="">Imagen</label>
-        <input type="file" name="imagen">
-        <button type="submit">Aceptar</button>            
-        </form>
+    {{-- <div class="form-group">
+            <form action="{{route('AgregarMascarilla')}}" method="POST" enctype="multipart/form-data">
+    @csrf
+    <label for="">Nombre de la mascarilla</label>
+    <input type="text">
+    <label for="">Categoria</label>
+    <input type="text">
+    <label for="">Precio</label>
+    <input type="text">
+    <label for="">Imagen</label>
+    <input type="file" name="imagen">
+    <button type="submit">Aceptar</button>
+    </form>
 
-    </div>
+</div> --}}
 
+<div class="row">
     @foreach ($mascarillas as $item)
-    <div class="card" style="width: 180px">
-        <div class="card-tittle">
-        <p>{{$item->nombre}}</p>
-        </div>
-        <div class="card-content">
-        <img class="img-fluid" src="{{asset($item->img)}}" alt="">
+    <div class="d-flex">
+        <div class="card m-2" style="">
+            <div class="text-center">
+                <img class="m-0 border-0 rounded-top img" src="{{asset($item->img)}}" alt="">
+            </div>
+            <div class="card-body">
+                <h2 class="card-title border-bottom">Card title</h2>
+                <div class="">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            Categoria
+                        </div>
+                        <div class="col-sm-6">
+                            Precio
+                        </div>
+                        <div class="col-sm-6">
+                            Cantidad
+                        </div>
+                    </div>
+                </div>
+                    <div class="text-center ">
+                        <div class="d-flex">
+                    <a href="#" class="mr-2 btn btn-primary">! Lo quiero ¡</a>
+                    <a href="#" class=" btn btn-outline-primary">Guardar en mis favoritos</a>  
+                        </div>
+  
+                    </div>
+        
+                </div>
         </div>
     </div>
 
     @endforeach
-
 </div>
+
 @endsection
