e9a9aba84380a556b7ded1f730064d9b3b2e1af8
ErickDArx <alexalonso2706@gmail.com>
2021-05-13 16:21:57 -0600

    Reinicio

--- routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-Route::get('/', 'MascarillasController@index');
-Route::post('/Agregar', 'MascarillasController@store')->name('AgregarMascarilla');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
