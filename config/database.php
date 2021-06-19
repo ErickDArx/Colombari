7897d988f8bd575bfa287198b2667d0f34582d22
ErickDArx <alexalonso2706@gmail.com>
2021-06-18 22:26:55 -0600

    V.1.13

--- config/database.php
+++ config/database.php
@@ -93,7 +93,7 @@ return [
 
         'mongodb' => [
             'driver' => 'mongodb',
-            'dsn'=> env('DB_DSN',''),
+            'host'=> env('DB_DSN',''),
             'database' => env('DB_DATABASE',''),
         ],
 
